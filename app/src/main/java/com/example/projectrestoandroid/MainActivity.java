3ddf78e19eea6976eba49950536cd90591fb68bc
edouard <ecornet@la-joliverie.com>
2023-02-27 15:21:48 +0100

    mise a jour, le "activity_main.xml" est mis a jour

--- app/src/main/java/com/example/projectrestoandroid/MainActivity.java
+++ app/src/main/java/com/example/projectrestoandroid/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.projectrestoandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
